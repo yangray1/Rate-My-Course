70dd4c2f4ccf7305acf361f0bf6c0a5a289e355d
Matthew Wong <matthew.jay.wong@gmail.com>
2018-12-02 10:24:47 -0500

    add modals

--- back-end/app/controllers/user.controller.js
+++ back-end/app/controllers/user.controller.js
@@ -1,4 +1,4 @@
-const User = require("../models/user.models");
+const User = require("../models/user.modals");
 
 newUser = (req, res) => {
   console.log(req.body);
---
+++ back-end/app/models/request-report.modals.js
@@ -0,0 +1,7 @@
+const mongoose = require('mongoose');
+
+const RequestReportSchema = mongoose.Schema({
+
+});
+
+module.exports = mongoose.model('RequestReport', RequestReportSchema);
